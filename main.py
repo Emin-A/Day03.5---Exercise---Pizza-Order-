acc7d13aaccb24974755c4683652afb1c45d4b20
Emin Avdovic <72197800+Emin-A@users.noreply.github.com>
2022-04-17 18:51:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+# Exercise - Pizza order 
+print("Welcome to Mario's Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L. ")
+S = 
+M = 
+L = 
+
+if size == S:
+  print("That will be $15!")
+if size == M:
+  print("That will be $20!")
+if size == L:
+  print("That will be $25!")
+
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+pepperoni_small = 
+pepperoni_medium_larger = 
+if add_pepperoni == pep:
+  print("")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+
