5ee897bdb89f48afe6e61f5d29c6e1ab43bdd491
Emin Avdovic <72197800+Emin-A@users.noreply.github.com>
2022-04-19 20:44:16 +0000

    Changed the if else structure... I am nearly close to finishing it.

--- main.py
+++ main.py
@@ -1,21 +1,40 @@
 # Exercise - Pizza order 
 print("Welcome to Mario's Pizza Deliveries!")
 size = input("What size pizza do you want? S, M, or L. ")
-S = 
-M = 
-L = 
 
-if size == S:
+if size == "S":
+  S = 15
   print("That will be $15!")
-if size == M:
+if size == "M":
+  M = 20
   print("That will be $20!")
-if size == L:
+if size == "L":
+  L = 25
   print("That will be $25!")
 
 add_pepperoni = input("Do you want pepperoni? Y or N ")
-pepperoni_small = 
-pepperoni_medium_larger = 
-if add_pepperoni == pep:
-  print("")
-extra_cheese = input("Do you want extra cheese? Y or N ")
+pepperoni_small = 2
+pepperoni_medium_large = 3
+if add_pepperoni == "Y":
+  if size == "S":
+    print(f"That will be ${S + pepperoni_small}.") 
+  if size == "M" or "L":
+    print(f"That will be ${(M or L) + pepperoni_medium_large}.")
 
+extra_cheese = input("Do you want extra cheese? Y or N ")
+any_size_cheese = 1
+if extra_cheese == "Y":
+  if size == "S":
+    if add_pepperoni == "Y":
+      print(f"That will be ${S + pepperoni_small + any_size_cheese}.")
+    else:
+      print(f"That will be ${S + any_size_cheese}.")
+  if size == "M" or "L":
+    if add_pepperoni == "Y":
+      print(f"That will be ${M or L + pepperoni_medium_large + any_size_cheese}.")
+    else:
+      print(f"That will be ${M or L + any_size_cheese}.")
+else:
+  print("Goodbyeee!")
+
+          
